a1f4b31177fd811cccbb29fa6d87b8acce57fdbd
Miguel Tang <A01610836@itesm.mx>
2022-06-10 19:00:48 -0500

    add mutex.cpp

---
+++ mutex.cpp
@@ -0,0 +1,59 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <pthread.h>
+
+int mails = 0;
+pthread_mutex_t mutex;
+
+void* routine(void* arg) {
+	for (int i = 0; i < 10000000; i++) {
+		pthread_mutex_lock(&mutex);
+		mails++;
+		pthread_mutex_unlock(&mutex);
+		// Read mails
+		// Increment the value
+		// Write mails back to memory
+
+		/*
+		When the threads starts running the program and
+		then after some time thread 2 stops while thread 1
+		keeps running, then at some point, when thread 2 starts
+		again, then the value gets restarted where thread 2
+		stopped.
+		*/
+	}
+}
+
+int main(int argc, char* argv[]) {
+	pthread_t t1, t2, t3, t4;
+	pthread_mutex_init(&mutex, NULL);
+	if (pthread_create(&t1, NULL, routine, NULL) != 0) {
+		return 1;
+	};
+	if (pthread_create(&t2, NULL, routine, NULL) != 0) {
+		return 2;
+	};
+	if (pthread_create(&t3, NULL, routine, NULL) != 0) {
+		return 1;
+	};
+	if (pthread_create(&t4, NULL, routine, NULL) != 0) {
+		return 2;
+	};
+	if (pthread_join(t1, NULL) != 0) {
+		return 3;
+	};
+	if (pthread_join(t2, NULL) != 0) {
+		return 4;
+	};
+	if (pthread_join(t3, NULL) != 0) {
+		return 5;
+	};
+	if (pthread_join(t4, NULL) != 0) {
+		return 6;
+	};
+	pthread_mutex_destroy(&mutex);
+	printf("Number of mails: %d\n", mails);
+	return 0;
+
+}
